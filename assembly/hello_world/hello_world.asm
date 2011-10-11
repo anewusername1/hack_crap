70b67b1a85a8aeaa5d25bdb8c52c47dc45a85a8b
Tracey Eubanks <traceyeubanks@yahoo.com>
2011-10-11 10:01:57 -0600

    compiled hello world

--- assembly/hello_world/hello_world.asm
+++ assembly/hello_world/hello_world.asm
@@ -1,26 +1,31 @@
-;  hello.asm  a first program for nasm for Linux, Intel, gcc
-;
-; assemble: nasm -f elf -l hello.lst  hello.asm
-; link:   gcc -o hello  hello.o
-; run:          hello
-; output is:  Hello World
+section     .text
+ global _start                       ;must be declared for linker (ld)
 
-  SECTION .data   ; data section
-msg:  db "Hello World",10 ; the string to print, 10=cr
-len:  equ $-msg   ; "$" means "here"
-        ; len is a value, not an address
+_syscall:           
+     int     0x80            ;system call
+     ret
 
-  SECTION .text   ; code section
-        global main   ; make label available to linker
-main:       ; standard  gcc  entry point
+_start:                         ;tell linker entry point
 
-  mov edx,len   ; arg3, length of string to print
-  mov ecx,msg   ; arg2, pointer to string
-  mov ebx,1   ; arg1, where to write, screen
-  mov eax,4   ; write sysout command to int 80 hex
-  int 0x80    ; interrupt 80 hex, call kernel
+     push    dword len       ;message length
+     push    dword msg       ;message to write
+     push    dword 1         ;file descriptor (stdout)
+     mov     eax,0x4         ;system call number (sys_write)
+     call    _syscall        ;call kernel
 
-  mov ebx,0   ; exit code, 0=normal
-  mov eax,1   ; exit command to kernel
-  int 0x80    ; interrupt 80 hex, call kernel
+                             ;the alternate way to call kernel:
+                             ;push   eax
+                             ;call   7:0
 
+     add     esp,12          ;clean stack (3 arguments * 4)
+
+     push    dword 0         ;exit code
+     mov     eax,0x1         ;system call number (sys_exit)
+     call    _syscall        ;call kernel
+
+                             ;we do not return from sys_exit,
+                             ;there's no need to clean stack
+section .data
+
+msg     db      "Hello, world!",0xa     ;our dear string
+len     equ     $ - msg                 ;length of our dear string
