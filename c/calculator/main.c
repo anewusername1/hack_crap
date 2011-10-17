37a2f3d8507dc9f1669d446216d6c8cbf6019ec3
Tracey Eubanks <traceyeubanks@yahoo.com>
2011-10-17 13:11:24 -0600

    beginnings of simple calculator

--- c/calculator/main.c
+++ c/calculator/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-void main(){
-
+int main(){
+  float num1, num2;
+  char operation;
+  while(1){
+    scanf("%f%c%f", &num1, &operation, &num2);
+    if(operation == '+')
+      printf("%f\n", num1 + num2);
+    if(operation == '/')
+      printf("%f\n", num1 / num2);
+    if(operation == '-')
+      printf("%f\n", num1 - num2);
+    if(operation == '*')
+      printf("%f\n", num1 * num2);
+  }
+  return 0;
 }
