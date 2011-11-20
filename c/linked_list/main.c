c273c981ffb05146bd50112b013f57e2df9124fd
tracey eubanks <traceyeubanks@biomac.local>
2011-11-20 09:41:57 -0700

    started creating linked list stuff in c

---
+++ c/linked_list/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+  char data;
+  struct node *next_el;
+}
+
+typedef struct node element;
+
+void main(){
+  node *curr, *head;
+  int i;
+  head = NULL;
+
+  for(i = 1; i <= 3; i++){
+
+  }
+}
