abec29c9eecbef4b414312b864b0b173340869e9
Tracey Eubanks <traceyeubanks@yahoo.com>
2011-11-27 21:05:36 -0700

    calculator doesn't work. saving it anyway..

--- c/calculator/main.c
+++ c/calculator/main.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
 
 char *get_expression(char *text, size_t size){
   size_t i = 0;
-  for(;;){
+  while(1){
     int ch = fgetc(stdin);
     if(ch == '\n' || ch == EOF)
       break;
@@ -10,17 +13,30 @@ char *get_expression(char *text, size_t size){
       text[i++] = ch;
   }
   text[i] = '\0';
+  if(strcmp(text, "exit\0"))
+    return "e";
+
   return text;
 }
 
 int main(){
   float num1, num2;
   char operation;
-  char expression[50];
+  char expression[51];
+  const int done = int('e');
   fputs("enter an expresson to be evaluated.\nCurrently supports +-/*\n", stdout);
   fflush(stdout);
   while(1){
     get_expression(expression, sizeof expression);
+    printf("done = %i; expression = %s; int(expression) = %i\n", done, expression, int(expression));
+    switch(atoi(expression)){
+      case done:
+        return(0);
+        break;
+      default:
+        fputs("Invalid expression\n", stdout);
+    }
+
     printf("%s\n", expression);
     /* scanf("%f%c%f", &num1, &operation, &num2); */
     /* if(operation == '+') */
